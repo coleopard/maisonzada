46a612d22f9216969f42e483610f41b3e76b57f9
Coralie Loheac <coralie.loheac@gmail.com>
2020-06-14 17:50:24 +0200

    toto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/menu', to: 'pages#menu'
+  get '/contact', to: 'pages#contact'
+
+  get '/contact', to: 'messages#new', as: 'new_message'
+  post 'messages', to: 'messages#create', as: 'create_message'
 end
+
+
+
